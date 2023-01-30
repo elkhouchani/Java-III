89110148899f12dc0716f50bd4fd2d84e70462d8
Siham EL KHOUCHANI <sihamelkhouchani@gmail.com>
2023-01-30 14:34:55 +0100

    SportsCar inherit from Car

---
+++ src/com/lq/demos/tranport/SportsCar.java
@@ -0,0 +1,29 @@
+package com.lq.demos.tranport;
+
+public class SportsCar extends Car {
+	
+//	Constructors from super class(Car)
+	
+	public SportsCar() {}	
+	
+	public SportsCar(String name) {
+		super(name);
+	}
+	
+	public SportsCar(int speed) {
+		super(speed);
+	}
+	
+	public SportsCar(String name,int speed, int fuel, boolean running){
+		super(name,speed,fuel,running);
+	}
+	
+	
+//	Methods from super class
+	@Override
+	// Override = Same signature
+	public void setSpeed(int newSpeed) {
+		super.setSpeed((int)(newSpeed*1.10));
+	}
+	
+}
